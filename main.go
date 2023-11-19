e3b919ceafb0410016cf29714bff644ce06bb0d4
Alexandre Hachey <alx99102@gmail.com>
2023-11-18 20:13:17 -0500

    failed templating

--- main.go
+++ main.go
@@ -48,12 +48,17 @@ func autoCompleteSearch(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(""))
 		return
 	}
+
+
+	// Call the autocomplete API
 	autoCompletesJsonData := callAutoComplete(location)
 	if autoCompletesJsonData == nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Error calling autocomplete API"))
 		return
 	}
+
+
 
 	// Unmarshal JSON into struct
 	var featureCollection FeatureCollection
@@ -63,27 +68,36 @@ func autoCompleteSearch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	addressStrings := []string{}
-
-	// Iterate through the features to extract the address
-	for _, feature := range featureCollection.Features {
-		addressStrings = append(addressStrings, feature.Properties.Formatted)
+	// Generate html fragment 
+	suggestionsMap := map[string][]Feature{
+		"suggestions": featureCollection.Features,
 	}
+	tmpl := template.Must(template.ParseFiles("location.html"))
+	tmpl.Execute(w, suggestionsMap)
+	
 
-	// Generate html fragment for each address
-	html := ""
-	for _, address := range addressStrings {
-		html += "<div>" + address + "</div>"
-	}
+	// addressStrings := []string{}
 
-	// Send the html fragment as response
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(html))
+	// // Iterate through the features to extract the address
+	// for _, feature := range featureCollection.Features {
+	// 	addressStrings = append(addressStrings, feature.Properties.Formatted)
+	// }
+
+	// // Generate html fragment for each address
+	// html := ""
+	// for _, address := range addressStrings {
+	// 	html += "<div>" + address + "</div>"
+	// }
+
+	// // Send the html fragment as response
+	// w.WriteHeader(http.StatusOK)
+	// w.Write([]byte(html))
+
 }
 
 func callAutoComplete(location string) []byte {
 	baseUrl := "https://api.geoapify.com/v1/geocode/autocomplete"
-	apiKey := ""
+	apiKey := "ba3ecb45c2e34a598c8fea9098d29d7a"
 	url := fmt.Sprintf("%s?text=%s&apiKey=%s", baseUrl, location, apiKey)
 
 	method := "GET"
